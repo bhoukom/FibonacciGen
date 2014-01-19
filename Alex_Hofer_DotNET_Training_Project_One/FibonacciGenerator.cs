2807037a273f0fd219cfd142ca935ad9acbb2076
Alex Hofer <ahofer@nerdery.com>
2014-01-19 14:00:07 -0600

    Initial commit, added basic Fibonacci functionality

---
+++ Alex_Hofer_DotNET_Training_Project_One/FibonacciGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alex_Hofer_DotNET_Training_Project_One
+{
+    internal static class FibonacciGenerator
+    {
+        //Making my Fibonacci Generator method so I can call it from where ever I want.
+        public static IEnumerable<int> FibGen(int n)
+        {
+            //Creating a list to store all the generated numbers in.
+            var _numberList = new List<int>();
+
+            //Setting my variables for the loop.
+            var first = 0;
+            var second = 1;
+
+            //Adding the first two numbers to the list because they will be changed, there's probably a better way to do this but for now it works.
+            _numberList.Add(first);
+            _numberList.Add(second);
+
+            //Array that will loop "n" times specified by the User to generate a list of Fibonacci numbers.
+            for (var i = 0; i < n; i++)
+            {
+                var final = second;
+
+                second = first + second;
+
+                first = final;
+
+                //Adding the newest Fibonacci number to the list.
+                _numberList.Add(second);
+            }
+
+            //Return my list to the main loop.
+            return _numberList;
+        }
+    }
+}
---
+++ Alex_Hofer_DotNET_Training_Project_One/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alex_Hofer_DotNET_Training_Project_One
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Stub for now, will be adding console parsing in the future.
+            Console.WriteLine("Please enter a number:");
+            var input = Console.ReadLine();
+            Console.Clear();
+            //End of stub
+
+            //Getting my input from the console, converting to an Int, then sending it off to the Fibonacci generator.
+            //Then getting back the list of numbers and storing it into "returnedNumbers".
+            var returnedNumbers = FibonacciGenerator.FibGen(Convert.ToInt32(input));
+
+            //Going though each number in the list and writing it to the console.
+            //NOTE: THe list will eventually be passed to one of three other methods that will output an XML File, Plain Text File, or just still write to the console.
+            foreach (var returnedNumber in returnedNumbers)
+            {
+                Console.WriteLine(returnedNumber);
+            }
+        }
+    }
+}
